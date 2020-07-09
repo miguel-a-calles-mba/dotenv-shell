4317ec255ccc5382fc7c3fb6a8ffb93c8bde297e
Miguel Calles <mcalles@veritoll.com>
2020-07-09 11:24:53 -0700

    Breaking changes
    - Only supporting the backtick format
    - Removed $(cmd) format
    - Added support for nested shell commands
    - Updated unit tests

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const { execSync } = require('child_process');
 
-const shellCmdPatterns = [/^\$\((.*)\)$/, /^`(.*)`$/];
+const shellCmdPattern = /`(.*)`/;
 const unsafeCmds = [
     /^rm/,
     /^:\(\){:\|:&};:/,
@@ -12,6 +12,11 @@ const unsafeCmds = [
     /^>/,
     /^mv/,
     /^\^/,
+    /^exit/,
+    /^kill/,
+    /^unlink/,
+    /^shutdown/,
+    /^reboot/,
 ];
 
 module.exports = (dotenvConfig) => {
@@ -24,29 +29,44 @@ module.exports = (dotenvConfig) => {
 };
 
 const shellExpand = (parsedConfig, loadEnv = false) => {
-    Object.entries(parsedConfig).forEach(([key, val]) => {
-        shellCmdPatterns.forEach((pattern) => {
-            const [, cmd] = pattern.exec(val) || [];
-            if (cmd) {
-                unsafeCmds.forEach((unsafeCmd) => {
-                    if (unsafeCmd.test(cmd)) {
-                        throw new Error(
-                            `${key}=${val} uses unsafe Linux command`,
-                        );
-                    }
-                });
-                try {
-                    const newVal = execSync(cmd, { env: process.env })
-                        .toString()
-                        .trim();
-                    parsedConfig[key] = newVal;
-                    if (loadEnv) {
-                        process.env[key] = newVal;
-                    }
-                } catch (e) {
-                    throw new Error(`Error for ${key}=${val}: ${e.message}`);
+    Object.entries(parsedConfig).forEach(([key, shellCmdString]) => {
+        try {
+            const [, cmd] = shellCmdPattern.exec(shellCmdString) || [];
+            unsafeCmds.forEach((unsafeCmd) => {
+                if (unsafeCmd.test(cmd)) {
+                    throw new Error(`unsafe Linux command`);
                 }
+            });
+            const newVal = expandShellCmd(shellCmdPattern, shellCmdString);
+            parsedConfig[key] = newVal;
+            if (loadEnv) {
+                process.env[key] = newVal;
             }
-        });
+        } catch (e) {
+            throw new Error(`Error for ${key}=${shellCmdString}: ${e.message}`);
+        }
     });
+};
+
+const expandShellCmd = (pattern, cmdString) => {
+    const cmdStringMatch = pattern.exec(cmdString);
+    if (cmdStringMatch) {
+        const replaceString = cmdStringMatch[0];
+        const subCmdString = cmdStringMatch[1];
+        const subCmdStringMatch = pattern.exec(subCmdString);
+        if (subCmdStringMatch) {
+            const expansion = execShellCmd(
+                expandShellCmd(pattern, subCmdString, true),
+            );
+            const output = cmdString.replace(replaceString, expansion);
+            return output;
+        }
+        const subCmdVal = execShellCmd(subCmdString);
+        return cmdString.replace(replaceString, subCmdVal);
+    }
+    return cmdString;
+};
+
+const execShellCmd = (cmd) => {
+    return execSync(cmd, { env: process.env }).toString().trim();
 };
--- test.js
+++ test.js
@@ -1,18 +1,43 @@
 'use strict';
 
+const rewire = require('rewire');
 const dotenv = require('dotenv');
 const dotenvExpand = require('dotenv-expand');
 const dotenvShell = require('./index');
+const execShellCmd = rewire('./index').__get__('execShellCmd');
+const expandShellCmd = rewire('./index').__get__('expandShellCmd');
+const shellCmdPattern = rewire('./index').__get__('shellCmdPattern');
 
+test('execShellCmd', () => {
+    expect(execShellCmd('echo 1')).toEqual('1');
+});
+describe('expandShellCmd', () => {
+    test('no shell cmd', () => {
+        expect(expandShellCmd(shellCmdPattern, 'echo 1')).toEqual('echo 1');
+    });
+    test('shell cmd', () => {
+        const cmd = '`echo basic`';
+        expect(expandShellCmd(shellCmdPattern, cmd)).toEqual('basic');
+    });
+    test('nested shell cmd, 1 level', () => {
+        const cmd = '`echo 1 `echo 2``';
+        expect(expandShellCmd(shellCmdPattern, cmd)).toEqual('1 2');
+    });
+    test('nested shell cmd, 2 levels', () => {
+        const cmd = '`echo 1 `echo 2 `echo 3```';
+        expect(expandShellCmd(shellCmdPattern, cmd)).toEqual('1 2 3');
+    });
+    test('nested shell cmd, 3 levels', () => {
+        const cmd = '`echo 1 `echo 2 `echo 3 `echo 4````';
+        expect(expandShellCmd(shellCmdPattern, cmd)).toEqual('1 2 3 4');
+    });
+});
 describe('shell expansion', () => {
     beforeEach(() => {
         process.env = {};
     });
-    test('expand $(cmd) from dotenv.parse', () => {
-        const buf = Buffer.from('BASIC=$(echo basic)');
-        const config = dotenv.parse(buf);
-        expect(dotenvShell(config)).toEqual({ BASIC: 'basic' });
-        expect(process.env).toEqual({});
+    afterEach(() => {
+        process.env = {};
     });
     test('expand `cmd` from dotenv.parse', () => {
         const buf = Buffer.from('BASIC=`echo basic`');
@@ -27,32 +52,40 @@ describe('shell expansion', () => {
         expect(process.env).toEqual({});
     });
     test('unsafe command in dotenv.parse', () => {
-        const cmd = '$(rm .)';
+        const cmd = '`rm .`';
         const buf = Buffer.from(`UNSAFE=${cmd}`);
         const config = dotenv.parse(buf);
-        expect(() => dotenvShell(config)).toThrowError(
-            new Error(`UNSAFE=${cmd} uses unsafe Linux command`),
-        );
+        const errMsg = `Error for UNSAFE=${cmd}: unsafe Linux command`;
+        expect(() => dotenvShell(config)).toThrowError(new Error(errMsg));
     });
     test('shell expansion from file', () => {
         const config = dotenv.config({ path: '.env.test' });
         const output = {
             BASIC: 'basic',
+            BASIC_ECHO: 'basic',
             BASIC_ENV: 'basic',
+            BASIC_ENV_1: 'basic',
+            BASIC_ENV_2: 'basic',
             EXPAND: 'expand',
             EXPANDED: '${EXPAND}',
             EXPANDED_ENV: 'expand',
         };
+        console.log('BEFORE', process.env);
+        dotenvShell(config);
         expect(dotenvShell(config)).toEqual({
             parsed: output,
         });
+        console.log('AFTER', process.env);
         expect(process.env).toEqual(output);
     });
     test('dotenv-expand => shell expansion from file', () => {
         const config = dotenv.config({ path: '.env.test' });
         const output = {
             BASIC: 'basic',
+            BASIC_ECHO: 'basic',
             BASIC_ENV: 'basic',
+            BASIC_ENV_1: 'basic',
+            BASIC_ENV_2: 'basic',
             EXPAND: 'expand',
             EXPANDED: 'expand',
             EXPANDED_ENV: 'expand',
@@ -66,7 +99,10 @@ describe('shell expansion', () => {
         const config = dotenv.config({ path: '.env.test' });
         const output = {
             BASIC: 'basic',
+            BASIC_ECHO: 'basic',
             BASIC_ENV: 'basic',
+            BASIC_ENV_1: 'basic',
+            BASIC_ENV_2: 'basic',
             EXPAND: 'expand',
             EXPANDED: 'expand',
             EXPANDED_ENV: 'expand',
@@ -80,8 +116,10 @@ describe('shell expansion', () => {
         test('from file', () => {
             const config = dotenv.config({ path: '.env.test1' });
             const output = {
-                VAR1: 'var1',
-                VAR1A: 'var1',
+                VAR_1: 'var1',
+                VAR_1_A: 'var1',
+                VAR_1_B: 'var1',
+                VAR_1_C: 'var1',
             };
             expect(dotenvShell(config)).toEqual({
                 parsed: output,
